429a6f1afb19eb61e5e14f786ce5e640757fd2eb
mpedone <mpedone@contents.com>
2025-10-19 14:51:57 +0200

    install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="bg-blue-500 text-white p-4">
+    Hello Tailwind!
+  </div>
+
 </template>
 
 <style scoped></style>
